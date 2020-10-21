87c42ac51228ddbcbd11b7a2b9b7e0e3217fc2b0
Arley Pereira <arley1995@ucl.br>
2020-10-20 21:10:29 -0300

    Bottom Navigation

--- app/src/main/java/com/example/olx/activity/MainActivity.kt
+++ app/src/main/java/com/example/olx/activity/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.olx.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.example.olx.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navView = findViewById<BottomNavigationView>(R.id.nav_view)
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        NavigationUI.setupWithNavController(navView, navController)
+
     }
 }
